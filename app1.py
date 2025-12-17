39efd24bee53ebdd76181103bd625e13d68361e0
Rokhaya Diop <rokhaya.diop@aims-senegal.org>
2025-12-17 09:18:54 +0000

    Adjust transition season multipliers for y_adj

--- app1.py
+++ app1.py
@@ -115,20 +115,20 @@ def ajuster_consommation(y_pred, df_test):
 
         # -------- Saison TRANSITION --------
         elif saison == "Transition":
-            if 0 <= h <= 4:
-                y_adj[i] *= 0.90
-            elif 5 <= h <= 8:
+            if 0 <= h <= 5:
+                y_adj[i] *= 0.97
+            elif 6 <= h <= 8:
                 y_adj[i] *= 1.09
             elif h== 9:        # pointe matin
-                y_adj[i] *= 0.92
+                y_adj[i] *= 1
             elif 10 <= h <= 13:
-                y_adj[i] *= 0.87
+                y_adj[i] *= 0.92
             elif 14 <= h <= 17:
-                y_adj[i] *= 0.86
+                y_adj[i] *= 0.94
             elif 18 <= h <= 21:      # pointe soir
-                y_adj[i] *= 0.95
+                y_adj[i] *= 0.98
             elif h >= 22:
-                y_adj[i] *= 0.95
+                y_adj[i] *= 0.92
         # -------- Saison HAUTE --------
         elif saison == "Haute":
             if 0 <= h <= 4:
