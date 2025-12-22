285906bb0c2412022a94647708af77cc7757f6e2
Rokhaya Diop <rokhaya.diop@aims-senegal.org>
2025-12-22 09:41:44 +0000

    Update app1.py

--- app1.py
+++ app1.py
@@ -74,7 +74,7 @@ def ajuster_consommation(y_pred, df_test):
     # Facteur température
     temp_factor = 1.0 + 0.085 * (df_test["temperature"] - df_test["temperature"].mean())
     # Facteur humidité
-    humid_factor = 1.0 - 0.001 * (df_test["humidity"] - df_test["humidity"].mean())
+    humid_factor = 1.0 - 0.01 * (df_test["humidity"] - df_test["humidity"].mean())
     # Facteur jours fériés
     ferie_factor = df_test["Date_only"].apply(facteur_ferie)
 
