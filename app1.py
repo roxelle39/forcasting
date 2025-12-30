90141e5cc1180db53a1657d59d709a997fd8ffb6
Rokhaya Diop <rokhaya.diop@aims-senegal.org>
2025-12-30 12:23:00 +0000

    Update app1.py

--- app1.py
+++ app1.py
@@ -109,7 +109,7 @@ def ajuster_consommation(y_pred, df_test):
             elif 17 <= h <= 18:      # journée calme
                 y_adj[i] *= 0.85  
             elif 19 <= h <= 22:      # pointe soir modérée
-                y_adj[i] *= 1.25
+                y_adj[i] *= 1.15
             elif h ==23 :
                 
                 y_adj[i] *= 0.99   
