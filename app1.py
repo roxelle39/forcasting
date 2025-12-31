841481d2bb4ece9275498907740b63c85280cdc4
Rokhaya Diop <rokhaya.diop@aims-senegal.org>
2025-12-31 09:38:43 +0000

    Update app1.py

--- app1.py
+++ app1.py
@@ -99,7 +99,7 @@ def ajuster_consommation(y_pred, df_test):
         # -------- Saison BASSE --------
         if saison == "Bas":
             if 0 <= h <= 8:          
-                y_adj[i] *= 1.4
+                y_adj[i] *= 1.38
             elif h ==  9:
                 y_adj[i] *= 1  
             elif h ==  10:  
